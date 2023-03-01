8dda1ad0e32a96cd4ee1d742ba1969c054beedda
DanTestehan <tdan89@yahoo.com>
2023-03-01 17:58:36 +0200

    dummy code to see that REST app works

---
+++ src/main/java/com/testehan/openliberty/rest/SystemApplication.java
@@ -0,0 +1,8 @@
+package com.testehan.openliberty.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class SystemApplication extends Application {
+}
